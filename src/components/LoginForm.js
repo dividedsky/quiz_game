80f0bb00b3024b8f3d17b960dc73843f061f4eb2
Justin <jdlow2@gmail.com>
2018-12-18 10:29:13 -0500

    login form clunky but complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,15 +16,24 @@ class LoginForm extends React.Component {
     this.setState({[e.target.name]: e.target.value});
   };
 
-  handleSubmit = e => {
+  handleRegister = e => {
+    console.log(e);
     e.preventDefault();
-    console.log(this.state);
     if (this.state.username && this.state.password && this.state.email) {
       localStorage.setItem('username', JSON.stringify(this.state.username));
       localStorage.setItem('password', JSON.stringify(this.state.password));
       localStorage.setItem('email', JSON.stringify(this.state.email));
+      window.location.reload();
     }
-    window.location.reload();
+  };
+
+  handleSignIn = e => {
+    e.preventDefault();
+    if (this.state.username && this.state.password) {
+      localStorage.setItem('username', JSON.stringify(this.state.username));
+      localStorage.setItem('password', JSON.stringify(this.state.password));
+      window.location.reload();
+    }
   };
 
   toggleRegister = () => {
@@ -50,22 +59,44 @@ class LoginForm extends React.Component {
   };
 
   render() {
+    if (this.state.register) {
+      return (
+        <>
+          <form
+            className="register-form"
+            onSubmit={this.handleRegister}
+            autoComplete="off">
+            <h3>{this.state.register ? 'Register' : 'Sign In'} to Play</h3>
+            {this.makeInput('username', 'text')}
+            {this.makeInput('password')}
+            {this.makeInput('email')}
+            {this.makeInput('imageUrl', 'text')}
+            <button className="btn submit-btn" type="submit">
+              Submit
+            </button>
+            <h3>or...</h3>
+            <button className="btn" onClick={this.toggleRegister}>
+              {this.state.register ? 'Sign In' : 'Sign Up'}
+            </button>
+          </form>
+        </>
+      );
+    }
     return (
       <>
-        <button className="btn" onClick={this.toggleRegister}>
-          register
-        </button>
         <form
           className="register-form"
-          onSubmit={this.handleSubmit}
+          onSubmit={this.handleSignIn}
           autoComplete="off">
-          <h3>Register to Play</h3>
+          <h3>{this.state.register ? 'Register' : 'Sign In'} to Play</h3>
           {this.makeInput('username', 'text')}
           {this.makeInput('password')}
-          {this.makeInput('email')}
-          {this.makeInput('imageUrl', 'text')}
           <button className="btn submit-btn" type="submit">
             Submit
+          </button>
+          <h3>or...</h3>
+          <button className="btn" onClick={this.toggleRegister}>
+            {this.state.register ? 'Sign In' : 'Sign Up'}
           </button>
         </form>
       </>
