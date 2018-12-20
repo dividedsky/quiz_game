5f8e1d001feec0e2add5a289ed31919e49ff3d38
dividedsky <jdlow2@gmail.com>
2018-12-19 21:19:20 -0500

    redirect to quizzes if quiz is null

--- src/containers/Quiz.js
+++ src/containers/Quiz.js
@@ -1,35 +1,18 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Quiz from '../components/Quiz';
 import {getQuizzes} from '../store/actions';
 
 class QuizContainer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      quiz: null,
-    };
-  }
-
-  componentDidMount() {
-    if (!this.props.quizzes.length) {
-      this.props.getQuizzes();
-    }
-  }
-  componentDidUpdate = () => {
-    if (this.props.quizzes.length && this.props.fetchingComplete) {
-      const quiz = this.props.quizzes.find(
-        q => String(q.id) === this.props.match.params.id,
-      );
-      this.setState({quiz: quiz});
-    }
-  };
-
   render() {
-    console.log('render', this.state);
+    const quiz = this.props.quizzes.find(
+      q => String(q.id) === this.props.match.params.id,
+    );
+    console.log('render', quiz);
 
-    if (this.state.quiz) return <Quiz quiz={this.state.quiz} />;
-    return <h3>loading</h3>;
+    if (quiz) return <Quiz quiz={quiz} />;
+    return <Redirect to="/quizzes" />;
   }
 }
 
